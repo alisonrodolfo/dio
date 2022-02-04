9046b47390de67b970486b5972e9ab43503a6a1a
Alison Barreiro <alison.rmb@gmail.com>
2022-02-04 19:14:43 -0300

    @TestConfiguration public class ProstgreSQL

---
+++ src/test/java/checkout/setup/ProstgreSQL.java
@@ -0,0 +1,24 @@
+package checkout.setup;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+/**
+ * @author https://github.com/alisonrodolfo
+ */
+@TestConfiguration
+public class ProstgreSQL {
+    /* Created by Alison on 04/02/2022 */
+
+    private static final PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
+            .withDatabaseName("checkout")
+            .withUsername("admin")
+            .withPassword("admin");
+
+    static {
+        postgreSQLContainer.start();
+        postgreSQLContainer.waitingFor(Wait.forHealthcheck());
+        System.setProperty("spring.datasource.url", postgreSQLContainer.getJdbcUrl());
+    }
+}
